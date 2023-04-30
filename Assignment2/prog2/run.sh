6eadf8370dd2bc15658ef49cd15356c6c5dba238
BernardoKaluza <bernardokaluza@ua.pt>
2023-04-30 18:13:06 +0100

    buffer underflow

--- Assignment2/prog2/run.sh
+++ Assignment2/prog2/run.sh
@@ -1,5 +1,8 @@
-mpicc -Wall -O3 -o ex2 ex2.c
+mpicc -Wall -O3 -g -o ex2 ex2.c merge.c
 rm -f ex2.80s*
 
-mpirun -n $1 ./ex2 $2 $3 $4 $5 $6 $7 $8 $9
+# mpirun -n $1 ./ex2 $2 xterm -e gdb ./ex2
+
+mpirun -np $1 xterm -e gdb -x commands.txt --args ./ex2 $2
+
 
