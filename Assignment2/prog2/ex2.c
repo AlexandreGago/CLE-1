21a9fb78a29d53132f58bfd1437ec611c3e9d056
BernardoKaluza <bernardokaluza@ua.pt>
2023-05-03 15:07:25 +0100

    ex2 perfect

--- Assignment2/prog2/ex2.c
+++ Assignment2/prog2/ex2.c
@@ -1,188 +1,163 @@
 #include <mpi.h>
-#include <stdbool.h>
+
 #include <stdio.h>
+
 #include <stdlib.h>
-#include <string.h>
+
+#include <math.h>
+
 #include <unistd.h>
-// #include "structs.h"
+
+#include <stdbool.h>
+
 #include <time.h>
+
 #include "merge.h"
 
+int main(int argc, char * argv[]) {
+    MPI_Comm presentComm, nextComm;
+    MPI_Group presentGroup, nextGroup;
+    int gMemb[8];
+    int rank, nProc, nProcNow, length, nIter;
+    int i, j;
 
-void processcmd(int argc, char* argv[]);
-int allFilesDone();
-int initializeDistributor(char **files, int numFilesa);
-
-MPI_Request dummyRequest = MPI_REQUEST_NULL;
-
-int main (int argc, char *argv[]){
-    int rank;
-    int size;
-    MPI_Init (&argc, &argv);
-    MPI_Comm_rank (MPI_COMM_WORLD, &rank);
-    MPI_Comm_size (MPI_COMM_WORLD, &size);
-    // MPI_Status status;
-
-    // int sub_arr_size;
-    // int *sub_arr;
-
-    //if size isn't 1,2,4 or 8 abort
-    if (size != 2 && size != 4 && size != 8 && size != 1){
-        printf("This exercice requires exactly 1,2,4 or 8 processes.\n");
-        MPI_Abort(MPI_COMM_WORLD, 1);
+    int err = MPI_Init( & argc, & argv);
+    if (err != MPI_SUCCESS) {
+        printf("Error initializing MPI\n");
+        MPI_Abort(MPI_COMM_WORLD, err);
     }
 
-    if (rank == 0){
+    MPI_Comm_rank(MPI_COMM_WORLD, & rank);
+    MPI_Comm_size(MPI_COMM_WORLD, & nProc);
 
-        printf ("initializing dispacher \n");
-        fflush(stdout);
-        //TODO read files from command line
-        char *filename = argv[1];
-        FILE *f; 
-        int *arr;
-        int arr_size;
+    if (rank == 0) {
+        if (nProc != 1 && nProc != 2 && nProc != 4 && nProc != 8) {
+            printf("Wrong number of processes!\n Should be 1, 2, 4 or 8\n");
+            MPI_Finalize();
+            return EXIT_FAILURE;
+        }
+        if (argc != 2) {
+            printf("Wrong number of arguments!\n Usage: mpirun -n %d %s <input file>\n", nProc, argv[0]);
+            MPI_Finalize();
+            return EXIT_FAILURE;
+        }
+    }
+
+    double start_time = MPI_Wtime();
+    nIter = 32 - __builtin_clz(nProc) ;
+    int * sendData;
+
+    if (rank == 0) {
+        char * filename = argv[1];
+        FILE * f;
 
         //open the file
         f = fopen(filename, "rb");
         if (f == NULL) {
-            printf("Error opening file %s   \n", filename);
-            exit(EXIT_FAILURE);
+            perror("Error opening file");
+            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+            return EXIT_FAILURE;
         }
         //read the number of elements
-        if(fread(&arr_size, sizeof(int), 1, f)<=0){
-            printf("Error reading file %s   \n", filename); 
-            exit(EXIT_FAILURE);
+        if (fread( & length, sizeof(int), 1, f) <= 0) {
+            perror("Error reading file");
+            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+            return EXIT_FAILURE;
         };
+
+        if (length <= 0) {
+            printf("Invalid length of the array: %d\n", length);
+            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+            return EXIT_FAILURE;
+        }
+
         //allocate memory for the array
-        arr = (int *)malloc(arr_size * sizeof(int));
+        sendData = malloc(length * sizeof(int));
+        if (sendData == NULL) {
+            perror("Error allocating memory");
+            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+            return EXIT_FAILURE;
+        }
+
         //read to the array
-        if(fread(arr, sizeof(int), arr_size, f)<=0){
-            printf("Error reading file %s   \n", filename);
-            exit(EXIT_FAILURE);
+        if (fread(sendData, sizeof(int), length, f) <= 0) {
+            perror("Error reading file");
+            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+            return EXIT_FAILURE;
         };
         fclose(f);
 
-        // printf ("dispacher read %d elements from file %s \n", arr_size, filename);
-        //send the size of the sub-array to all workers
-        int sub_array_size = arr_size/(size);
-        MPI_Bcast(&sub_array_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        MPI_Bcast(&arr_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast( & length, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-        //send the sub-arrays to the workers
-        int *sub_array = (int *)malloc(sub_array_size * sizeof(int));
-        MPI_Scatter(arr, sub_array_size, MPI_INT, sub_array, sub_array_size, MPI_INT, 0, MPI_COMM_WORLD);
+    } else {
+        MPI_Bcast( & length, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        sendData = NULL;
+    }
 
-        // mergeSort
-        mergeSortItr(sub_array, sub_array_size);
+    int * recData = malloc(length * sizeof(int));
+    if (recData == NULL) {
+        perror("Error allocating memory");
+        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+        return EXIT_FAILURE;
+    }
 
-
-        MPI_Gather(sub_array, sub_array_size, MPI_INT, arr, sub_array_size, MPI_INT, 0, MPI_COMM_WORLD);
-        
-
-
-        free(sub_array);
-
-
-        //merge part
-        while (sub_array_size < arr_size){
-            sub_array_size = sub_array_size*2;
-            printf("#####Scatter#####\n");
-
-            //send the sub-arrays to the workers
-            printf("Malloc sub_array\n");
-            fflush  (stdout);
-            int *sub_array = (int *)malloc(sub_array_size * sizeof(int));
-            printf("Malloc sub_array after\n");
-            fflush  (stdout);
-            
-            MPI_Scatter(arr, sub_array_size, MPI_INT, sub_array, sub_array_size, MPI_INT, 0, MPI_COMM_WORLD);
-            printf("Scatter after");
-            fflush  (stdout);
-            mergeItr(sub_array,0,sub_array_size/2-1,sub_array_size-1);
-
-            MPI_Gather(sub_array, sub_array_size, MPI_INT, arr, sub_array_size, MPI_INT, 0, MPI_COMM_WORLD);
-            printf("#####Gather 2nd time#####");
-            fflush  (stdout);
-
-        }
-            
-        //check if the array is sorted
-        bool sorted = true;
-        for (int i = 0; i < arr_size-1; i++) {
-            if (arr[i] > arr[i+1]) {
-                sorted = false;
-                break;
+    nProcNow = nProc;
+    presentComm = MPI_COMM_WORLD;
+    MPI_Comm_group(presentComm, & presentGroup);
+    for (i = 0; i < 8; i++)
+        gMemb[i] = i;
+    for (j = 0; j < nIter; j++) {
+        if (j > 0) {
+            MPI_Group_incl(presentGroup, nProcNow, gMemb, & nextGroup);
+            MPI_Comm_create(presentComm, nextGroup, & nextComm);
+            presentGroup = nextGroup;
+            presentComm = nextComm;
+            if (rank >= nProcNow) {
+                free(recData);
+                MPI_Finalize();
+                return EXIT_SUCCESS;
             }
         }
-        if (sorted) {
-            printf("Array is sorted\n");
-        }
-        else {
-            printf("Array is not sorted\n");
+
+        MPI_Comm_size(presentComm, & nProc);
+        MPI_Scatter(sendData, length / nProcNow, MPI_INT, recData, length / nProcNow, MPI_INT, 0, presentComm);
+
+        if (j == 0)
+            mergeSortItr(recData, length / nProcNow);
+        else
+            mergeItr(recData, 0, (length / nProcNow) / 2 - 1, (length / nProcNow) - 1);
+
+        MPI_Gather(recData, length / nProcNow, MPI_INT, sendData, length / nProcNow, MPI_INT, 0, presentComm);
+        nProcNow = nProcNow >> 1;
+    }
+
+    //stop the timer
+    double end_time = MPI_Wtime();
+    double elapsed_time = end_time - start_time;
+
+    //check if the array is sorted
+    bool sorted = true;
+    for (int i = 0; i < length - 1; i++) {
+        if (sendData[i] > sendData[i + 1]) {
+            sorted = false;
+            break;
         }
     }
 
-    else {
-        int sub_array_size;
-        int arr_size;
-        printf("worker %d initiated\n", rank);
-        fflush(stdout);
-
-        //! receive the size of the sub-array
-        MPI_Bcast(&sub_array_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        MPI_Bcast(&arr_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        int *sub_array = (int *)malloc(sub_array_size * sizeof(int));
-
-        //receive the sub-array
-        MPI_Scatter(NULL, sub_array_size, MPI_INT, sub_array, sub_array_size, MPI_INT, 0, MPI_COMM_WORLD);
-        mergeSortItr(sub_array, sub_array_size);
-        printf("Rank %d sorted sub-array\n", rank);
-        //send the sub-array back
-        MPI_Gather(sub_array, sub_array_size, MPI_INT, NULL, sub_array_size, MPI_INT, 0, MPI_COMM_WORLD);
-        free(sub_array);
-        while(sub_array_size < arr_size){//merge part
-
-            sub_array_size = sub_array_size*2;
-            
-            //if rank is superior or equal to the number of sub-arrays, exit (32/32 = 1; the only worker would be the dispacher so the worker needes to exit)
-            if (rank >= (arr_size/sub_array_size)){
-                int *sub_array = (int *)malloc(sub_array_size * sizeof(int));
-                //TODO SEND NOTHING
-
-                MPI_Scatter(NULL, 0, MPI_INT, sub_array, sub_array_size, MPI_INT, 0, MPI_COMM_WORLD);
-                printf("Rank %d received sub-array size: %d\n", rank, sub_array_size);
-                MPI_Gather(sub_array, sub_array_size, MPI_INT, NULL, 0, MPI_INT, 0, MPI_COMM_WORLD);
-
-                free(sub_array);
-            }
-            else{
-                int *sub_array = (int *)malloc(sub_array_size * sizeof(int));
-
-                //receive the sub-array
-                MPI_Scatter(NULL, sub_array_size, MPI_INT, sub_array, sub_array_size, MPI_INT, 0, MPI_COMM_WORLD);
-                printf("Rank %d received sub-array size: %d\n", rank, sub_array_size);
-               
-                //merge
-                mergeItr(sub_array,0,sub_array_size/2-1,sub_array_size-1);
-
-                //send the merged sub-array
-                MPI_Gather(sub_array, sub_array_size, MPI_INT, NULL, sub_array_size, MPI_INT, 0, MPI_COMM_WORLD);
-                free(sub_array);
-            }
-
-        }
+    if (sorted) {
+        printf("Array is sorted\n");
+    } else {
+        printf("Array is not sorted\n");
     }
 
-    MPI_Finalize ();
+    free(sendData);
+    free(recData);
+
+    if (rank == 0) {
+        printf("Time elapsed: %f seconds\n", elapsed_time);
+    }
+
+    MPI_Finalize();
+
     return EXIT_SUCCESS;
 }
-
-
-int initializeDistributor(char **files, int numFiles) {
-
-
-
-    return EXIT_SUCCESS;
-}
-
-
--- Assignment2/prog2/run.sh
+++ Assignment2/prog2/run.sh
@@ -1,8 +1,14 @@
-mpicc -Wall -O3 -g -o ex2 ex2.c merge.c
+mpicc -Wall -O3 -g -o ex2 ex2.c merge.c -lm
 rm -f ex2.80s*
 
-# mpirun -n $1 ./ex2 $2 xterm -e gdb ./ex2
+# mpirun -n $1 ./ex2 $2
 
-mpirun -np $1 xterm -e gdb -x commands.txt --args ./ex2 $2
+mpirun -n 1 ./ex2 $2
+mpirun -n 2 ./ex2 $2
+mpirun -n 4 ./ex2 $2
+mpirun -n 8 ./ex2 $2
+
+
+# mpirun -np $1 xterm -e gdb -x commands.txt --args ./ex2 $2
 
 
